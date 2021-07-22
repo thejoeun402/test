2db7898065a7bb307b96b9d07504a0d79e141330
rlawn <rlawn@DESKTOP-57H71PO>
2021-07-22 11:29:20 +0900

    2021072211_세번째 커밋 & 푸쉬

--- src/com/greedy/git/Application.java
+++ src/com/greedy/git/Application.java
@@ -3,9 +3,9 @@ package com.greedy.git;
 public class Application {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world!");
 		
 		System.out.println("오~~ 깃허브에 뭔가 올라 가는데?");
+		System.out.println("한줄 추가하기");
 	}
 
 }
